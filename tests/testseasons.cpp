2ca605e99dcebaef0e2bb75c229bb44a721127e8
Allen Winter <winter@kde.org>
2006-04-25 15:35:21 +0000

    Change the license to LGPL. Also updated the copyright year(s).
    svn path=/trunk/KDE/kdepim/libkholidays_ng/tests/; revision=533793

--- tests/testseasons.cpp
+++ tests/testseasons.cpp
@@ -1,7 +1,6 @@
 /*
     This file is part of libkholidays.
-
-    Copyright (c) 2004 Allen Winter <winter@kde.org>
+    Copyright (c) 2004,2006 Allen Winter <winter@kde.org>
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Library General Public
@@ -15,7 +14,7 @@
 
     You should have received a copy of the GNU Library General Public License
     along with this library; see the file COPYING.LIB.  If not, write to
-    the Free Software Foundation, Inc., 51 Franklin Steet, Fifth Floor,
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
     Boston, MA 02110-1301, USA.
 */
 
