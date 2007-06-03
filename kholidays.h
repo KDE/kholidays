2c62ea8a29a55e9482b6ac3696509a1f4ae429fa
Allen Winter <winter@kde.org>
2007-06-03 18:58:53 +0000

    relicense my code from GPL+Qt exception to LGPL. reformat the license header too.
    svn path=/trunk/KDE/kdepim/libkholidays/; revision=671086

--- kholidays.h
+++ kholidays.h
@@ -1,21 +1,21 @@
 /*
-    This file is part of KOrganizer.
-    Copyright (c) 2001 Cornelius Schumacher <schumacher@kde.org>
-    Copyright (c) 2004 Allen Winter <winter@kde.org>
+  This file is part of KOrganizer.
+  Copyright (c) 2001 Cornelius Schumacher <schumacher@kde.org>
+  Copyright (c) 2004 Allen Winter <winter@kde.org>
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-    GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software  Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 */
 #ifndef KHOLIDAYS_HOLIDAYS_H
 #define KHOLIDAYS_HOLIDAYS_H
@@ -49,7 +49,7 @@ class KHOLIDAYS_EXPORT KHolidays {
     QString location() const;
 
     QList<KHoliday> getHolidays( const QDate& );
-    
+
     KDE_DEPRECATED QString shortText( const QDate& );
     KDE_DEPRECATED QString getHoliday( const QDate& );
 
