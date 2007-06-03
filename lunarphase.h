12fc6e9ced5a0a7f25bcd75955b501e5e69cf374
Allen Winter <winter@kde.org>
2007-06-03 18:54:00 +0000

    namespacing
    svn path=/trunk/KDE/kdepim/libkholidays/; revision=671085

--- lunarphase.h
+++ lunarphase.h
@@ -1,31 +1,20 @@
 /*
-    This file is part of libkholidays.
-    Copyright (c) 2004 Allen Winter <winter@kde.org>
+  This file is part of libkholidays.
+  Copyright (c) 2004,2007 Allen Winter <winter@kde.org>
 
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU General Public License as
-    published by the Free Software Foundation; either version 2 of the
-    License, or (at your option) any later version.
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License as
+  published by the Free Software Foundation; either version 2 of the
+  License, or (at your option) any later version.
 
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    General Public License for more details.
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
-
-    In addition, as a special exception, the copyright holders give
-    permission to link the code of this program with any edition of
-    the Qt library by Trolltech AS, Norway (or with modified versions
-    of Qt that use the same license as Qt), and distribute linked
-    combinations including the two.  You must obey the GNU General
-    Public License in all respects for all of the code used other than
-    Qt.  If you modify this file, you may extend this exception to
-    your version of the file, but you are not obligated to do so.  If
-    you do not wish to do so, delete this exception statement from
-    your version.
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 #ifndef KHOLIDAYS_LUNARPHASE_H
@@ -57,15 +46,19 @@
    Note that crescent and gibbous phases are not currently supported.
 */
 
+#include "kholidays_export.h"
+
 #include <QString>
 #include <QDateTime>
 
 #include <time.h>
 
-class LunarPhase
+namespace LibKHolidays {
+
+class KHOLIDAYS_EXPORT LunarPhase
 {
   public:
-    /** 
+    /**
       Phases of the moon, in traditional English notation. The
       phase @c None is used only as an error indicator, for instance
       in phase().
@@ -80,7 +73,7 @@ class LunarPhase
       Constructor. Sets the hemisphere for phase calculations
       to @p hemisphere.
 
-      @param hemisphere Hemisphere of the earth for which the phase 
+      @param hemisphere Hemisphere of the earth for which the phase
                          should be computed.
     */
     LunarPhase( Hemisphere hemisphere = Northern );
@@ -163,6 +156,6 @@ private:
     Hemisphere mHemisphere;
 };
 
+}
+
 #endif
-
-
