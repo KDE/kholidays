23df013fd38468698dd58c03b2c51be4fbd7ba48
Allen Winter <winter@kde.org>
2007-06-07 20:24:31 +0000

    change license to LGPL. Approval granted by Cornelius.
    svn path=/trunk/KDE/kdepim/libkholidays/; revision=672669

--- kholidays.cpp
+++ kholidays.cpp
@@ -1,30 +1,31 @@
 /*
-  This file is part of KOrganizer.
+  This file is part of libkholidays.
   Copyright (c) 2001 Cornelius Schumacher <schumacher@kde.org>
   Copyright (c) 2004 Allen Winter <winter@kde.org>
 
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
-  (at your option) any later version.
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Library General Public
+  License as published by the Free Software Foundation; either
+  version 2 of the License, or (at your option) any later version.
 
-  This program is distributed in the hope that it will be useful,
+  This library is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-  GNU General Public License for more details.
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Library General Public License for more details.
 
-  You should have received a copy of the GNU General Public License along
-  with this program; if not, write to the Free Software Foundation, Inc.,
-  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+  You should have received a copy of the GNU Library General Public License
+  along with this library; see the file COPYING.LIB.  If not, write to the
+  Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+  Boston, MA 02110-1301, USA.
 */
+
+#include "kholidays.h"
+#include "kholidays_version.h"
 
 #include <QFile>
 #include <kapplication.h>
 #include <kstandarddirs.h>
 #include <kdebug.h>
-
-#include "kholidays.h"
-#include "kholidays_version.h"
 
 extern "C" {
   char *parse_holidays( const char *, int year, short force );
