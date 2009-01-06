d581e6562adcf6e66e0520191801f598df5389d3
Dirk Mueller <mueller@kde.org>
2009-01-06 17:13:38 +0000

    branching KDE 4.2
    svn path=/branches/KDE/4.2/kdepimlibs/; revision=906699

--- config-libkholidays.h.cmake
+++
@@ -1,8 +0,0 @@
-/* Define to 1 if you have the <err.h> header file. */
-#cmakedefine HAVE_ERR_H 1
-
-/* Define to 1 if you have the <malloc.h> header file. */
-#cmakedefine HAVE_MALLOC_H 1
-
-/* Define to 1 if you have the <sys/cdefs.h> header file. */
-#cmakedefine HAVE_SYS_CDEFS_H 1
